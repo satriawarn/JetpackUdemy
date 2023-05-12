45d25c9c10c597702a2da58ec6af0eb357f40eef
satriawarn <satriawarn@gmail.com>
2023-05-12 23:20:27 +0700

    Dynamic content example

--- app/src/main/java/com/eric/jetpackudemy/MainActivity.kt
+++ app/src/main/java/com/eric/jetpackudemy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.eric.jetpackudemy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,23 +13,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.eric.jetpackudemy.ui.theme.JetpackUdemyTheme
 
+val nameList: List<String> = listOf("John", "Michael", "Andrew", "Danna")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackUdemyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            GreetingList(names = nameList)
         }
     }
 }
 
+@Composable
+fun GreetingList(names: List<String>){
+    Column{
+        for (name in names){
+            Greeting(name = name)
+        }
+    }
+
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -40,7 +43,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetpackUdemyTheme {
-        Greeting("Android")
-    }
+    GreetingList(names = nameList  )
 }
