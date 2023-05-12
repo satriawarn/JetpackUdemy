78f2167b99bedee8dce3d458b11ec18334f31f20
satriawarn <satriawarn@gmail.com>
2023-05-12 23:30:45 +0700

    Recomposition

--- app/src/main/java/com/eric/jetpackudemy/MainActivity.kt
+++ app/src/main/java/com/eric/jetpackudemy/MainActivity.kt
@@ -3,17 +3,22 @@ package com.eric.jetpackudemy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.eric.jetpackudemy.ui.theme.JetpackUdemyTheme
 
-val nameList: List<String> = listOf("John", "Michael", "Andrew", "Danna")
+val nameList: ArrayList<String> = arrayListOf("John", "Michael", "Andrew", "Danna")
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +29,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingList(names: List<String>){
-    Column{
-        for (name in names){
+fun GreetingList(names: List<String>) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        for (name in names) {
             Greeting(name = name)
+        }
+        Button(onClick = { nameList.add("Eric") }) {
+            Text(text = "Add New Name")
         }
     }
 
 }
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        style = MaterialTheme.typography.headlineLarge,
         modifier = modifier
     )
 }
@@ -43,5 +57,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    GreetingList(names = nameList  )
+    GreetingList(names = nameList)
 }
