6249d5072ee4b3aafb9248f940604decc13feb51
satriawarn <satriawarn@gmail.com>
2023-05-16 23:24:03 +0700

    add textfield with state

--- app/src/main/java/com/eric/jetpackudemy/MainActivity.kt
+++ app/src/main/java/com/eric/jetpackudemy/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.eric.jetpackudemy
 
 import android.annotation.SuppressLint
@@ -8,10 +10,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,23 +37,37 @@ fun MainScreen() {
         mutableStateListOf<String>("John", "Lennon")
     }
 
+    val newNameStateContent = remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        GreetingList(greetingListState) {
-            greetingListState.add("Eric")
-        }
+        GreetingList(greetingListState, {
+            greetingListState.add(newNameStateContent.value )
+        }, newNameStateContent.value, { newName ->
+            newNameStateContent.value = newName
+        })
     }
 }
 
 @Composable
-fun GreetingList(nameList: List<String>, buttonClick: () -> Unit) {
+fun GreetingList(
+    nameList: List<String>,
+    buttonClick: () -> Unit,
+    textFieldValue: String,
+    textFieldUpdate: (newName: String) -> Unit
+) {
     for (name in nameList) {
         Greeting(name = name)
     }
-    Button(onClick = buttonClick ) {
+
+    TextField(value = textFieldValue, onValueChange = textFieldUpdate)
+
+    Button(onClick = buttonClick) {
         Text(text = "Add New Name")
     }
 }
