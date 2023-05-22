4c39110af78113515c8a2fdd597ad455bc631a82
satriawarn <satriawarn@gmail.com>
2023-05-22 22:53:55 +0700

    add scaffold and topAppBar

--- app/src/main/java/com/eric/jetpackudemy/MainActivity.kt
+++ app/src/main/java/com/eric/jetpackudemy/MainActivity.kt
@@ -1,5 +1,8 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.eric.jetpackudemy
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,45 +16,66 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.ProvidedValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.eric.jetpackudemy.ui.theme.JetpackUdemyTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.TopAppBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            JetpackUdemyTheme() {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    Scaffold(topBar = { AppBar() }) {
+        Surface(
+            modifier = Modifier.fillMaxSize().padding(it),
+            color = Color.LightGray
+        ) {
+            ProfileCard()
         }
     }
 }
 
 @Composable
-fun MainScreen() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color.LightGray
-    ) {
-        ProfileCard()
-    }
+fun AppBar(){
+    TopAppBar(
+        title = { Text("Jetpack Application") },
+        navigationIcon = {
+            IconButton(onClick = { /* Do something */ }) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "BACK")
+            }
+        }
+    )
 }
 
 @Composable
@@ -61,7 +85,8 @@ fun ProfileCard() {
             .padding(16.dp)
             .fillMaxWidth()
             .wrapContentHeight(align = Alignment.Top),
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(Color.White)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -83,7 +108,7 @@ fun ProfilePicture() {
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
+            painter = painterResource(id = R.drawable.unicorn),
             contentDescription = "blablalba",
             modifier = Modifier.size(72.dp),
             contentScale = ContentScale.Crop
@@ -109,5 +134,7 @@ fun ProfileContent() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MainScreen()
+    JetpackUdemyTheme() {
+        MainScreen()
+    }
 }
