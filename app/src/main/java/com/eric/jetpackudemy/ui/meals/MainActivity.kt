5693276b421a2e3c8b379cc79aa99cc76e452c97
satriawarn <satriawarn@gmail.com>
2023-05-30 22:48:18 +0700

    add callback for network response

--- app/src/main/java/com/eric/jetpackudemy/ui/meals/MainActivity.kt
+++ app/src/main/java/com/eric/jetpackudemy/ui/meals/MainActivity.kt
@@ -3,10 +3,16 @@ package com.eric.jetpackudemy.ui.meals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.eric.jetpackudemy.model.response.MealsResponse
 import com.eric.jetpackudemy.ui.theme.JetpackUdemyTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +29,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MealsCategoriesScreen() {
     val viewModel: MealsCategoriesViewModel = viewModel()
-    val meals = viewModel.getMeals()
-    Text(
-        text = meals.toString(),
-    )
+    val rememberedMeals: MutableState<List<MealsResponse>> =
+        remember { mutableStateOf(emptyList()) }
+    viewModel.getMeals { response ->
+        val mealsFromTheApi = response?.categories
+        rememberedMeals.value = mealsFromTheApi.orEmpty()
+    }
+
+    LazyColumn{
+        items(rememberedMeals.value){meals ->
+            Text(
+                text = meals.name
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/eric/jetpackudemy/ui/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/eric/jetpackudemy/ui/meals/MealsCategoriesViewModel.kt
@@ -2,11 +2,14 @@ package com.eric.jetpackudemy.ui.meals
 
 import androidx.lifecycle.ViewModel
 import com.eric.jetpackudemy.model.MealsRepository
+import com.eric.jetpackudemy.model.response.MealsCategoriesResponse
 import com.eric.jetpackudemy.model.response.MealsResponse
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()) :
     ViewModel() {
-        fun getMeals(): List<MealsResponse>{
-            return repository.getMeals()?.categories.orEmpty()
+    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit) {
+        repository.getMeals { response ->
+            successCallback(response)
         }
+    }
 }
