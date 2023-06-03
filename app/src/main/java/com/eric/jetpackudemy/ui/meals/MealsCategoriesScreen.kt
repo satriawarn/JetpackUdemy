dd051d46403890468849e13182821daefbf69536
satriawarn <satriawarn@gmail.com>
2023-06-03 14:30:06 +0700

    implement expanding list items

--- app/src/main/java/com/eric/jetpackudemy/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/com/eric/jetpackudemy/ui/meals/MealsCategoriesScreen.kt
@@ -1,6 +1,8 @@
 package com.eric.jetpackudemy.ui.meals
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,19 +12,28 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import com.eric.jetpackudemy.model.response.MealsResponse
 import com.eric.jetpackudemy.ui.theme.JetpackUdemyTheme
+import androidx.compose.runtime.*
 
 @Composable
 fun MealsCategoriesScreen() {
@@ -37,6 +48,9 @@ fun MealsCategoriesScreen() {
 
 @Composable
 fun MealCategory(meal: MealsResponse) {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(
@@ -47,21 +61,42 @@ fun MealCategory(meal: MealsResponse) {
             .fillMaxWidth()
             .padding(top = 16.dp)
     ) {
-        Row {
+        Row(modifier = Modifier.animateContentSize()) {
             Image(
                 painter = rememberAsyncImagePainter(model = meal.imageUrl),
                 contentDescription = null,
                 modifier = Modifier
                     .size(88.dp)
                     .padding(4.dp)
+                    .align(Alignment.CenterVertically)
             )
             Column(
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
+                    .fillMaxWidth(0.8f)
                     .padding(16.dp)
             ) {
-                Text(text = meal.name, style = MaterialTheme.typography.bodyMedium)
+                Text(text = meal.name, style = MaterialTheme.typography.titleMedium)
+                CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onSurfaceVariant) {
+                    Text(
+                        text = meal.description,
+                        style = MaterialTheme.typography.bodyMedium,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = if (isExpanded) 10 else 3
+                    )
+                }
+
             }
+            Icon(
+                imageVector = if (isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "expand_row_icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(if (isExpanded) Alignment.Bottom else Alignment.CenterVertically)
+                    .clickable {
+                        isExpanded = !isExpanded
+                    }
+            )
         }
 
     }
