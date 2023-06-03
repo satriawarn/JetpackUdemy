d004075498c247c160c6147e07727423e8d101ca
satriawarn <satriawarn@gmail.com>
2023-06-03 23:08:37 +0700

    collapsing toolbar on Column

--- app/src/main/java/com/eric/jetpackudemy/ui/meals/details/MealDetailsScreen.kt
+++ app/src/main/java/com/eric/jetpackudemy/ui/meals/details/MealDetailsScreen.kt
@@ -1,81 +1,94 @@
 package com.eric.jetpackudemy.ui.meals.details
 
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.animateDp
-import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import coil.compose.rememberAsyncImagePainter
 import com.eric.jetpackudemy.model.response.MealsResponse
+import java.lang.Float.min
 
 
 @Composable
 fun MealDetailScreen(meal: MealsResponse?) {
-    var profilePictureState by remember { mutableStateOf(MealProfilePictureState.Normal) }
-    val transition = updateTransition(targetState = profilePictureState, label = "")
-    val imageSizeDp by transition.animateDp(
-        targetValueByState = {
-            it.size
-        }, label = ""
-    )
-    val color by transition.animateColor(targetValueByState = { it.color }, label = "")
-    val widthSize by transition.animateDp(targetValueByState = { it.borderWidth }, label = "")
+    val scrollState = rememberScrollState()
+    val offSet = min(1f, 1 - (scrollState.value / 600f))
+    val size by animateDpAsState(targetValue = max(100.dp, 200.dp * offSet))
 
-    Column {
-        Row {
-            Card(
-                modifier = Modifier.padding(16.dp),
-                shape = CircleShape,
-                border = BorderStroke(width = widthSize, color = color)
-            ) {
-                Image(
-                    painter = rememberAsyncImagePainter(model = meal?.imageUrl),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(imageSizeDp)
-                        .clip(CircleShape)
-                        .padding(8.dp)
-                )
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Column {
+            Surface(shadowElevation = 4.dp) {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Card(
+                        modifier = Modifier.padding(16.dp),
+                        shape = CircleShape,
+                        border = BorderStroke(width = 2.dp, color = Color.Green)
+                    ) {
+                        Image(
+                            painter = rememberAsyncImagePainter(model = meal?.imageUrl),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(size)
+                                .clip(CircleShape))
+                    }
+                    Text(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .align(Alignment.CenterVertically),
+                        text = meal?.name ?: "default name",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
             }
-            Text(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(Alignment.CenterVertically),
-                text = meal?.name ?: "default name",
-                style = MaterialTheme.typography.bodyLarge
-            )
+
+            Column(modifier = Modifier
+                .verticalScroll(scrollState)
+                .fillMaxWidth()) {
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+                Text(text = "asdasdasda", modifier = Modifier.padding(16.dp))
+            }
         }
-        Button(modifier = Modifier.padding(16.dp), onClick = {
-            profilePictureState = if (profilePictureState == MealProfilePictureState.Normal)
-                MealProfilePictureState.Expanded
-            else
-                MealProfilePictureState.Normal
-        }) {
-            Text(
-                text = "Change state of meal profile picture",
-                style = MaterialTheme.typography.bodyLarge
-            )
-        }
+
+
     }
 }
 
